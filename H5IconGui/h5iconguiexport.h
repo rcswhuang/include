8c4ed30d1524a51f18e17849ebbfd1b049c07a7e
rcswhuang <rcswhuang@163.com>
2017-10-23 11:21:03 +0800

    Signed-off-by: rcswhuang <rcswhuang@163.com>

---
+++ H5IconGui/h5iconguiexport.h
@@ -0,0 +1,15 @@
+﻿#ifndef H5ICONGUI_EXPORT_H
+#define H5ICONGUI_EXPORT_H
+
+
+#if defined(WIN32)
+#if defined(H5ICONGUI_DLL)
+    #define H5ICONGUI_EXPORT __declspec(dllexport)
+#else
+    #define H5ICONGUI_EXPORT __declspec(dllimport)
+#endif
+#else
+    #define H5ICONGUI_EXPORT
+#endif
+//
+#endif 
