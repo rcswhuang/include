cce872fb5aafa77b72552685ad075c29b963b3d2
huagnw <rcswhuang@163.com>
2017-11-29 22:37:05 +0800

    on mac

--- H5IconGui/hiconitemgroup.h
+++ H5IconGui/hiconitemgroup.h
@@ -1,12 +1,30 @@
 ﻿#ifndef HICONITEMGROUP_H
 #define HICONITEMGROUP_H
+#include <QtGlobal>
+#include "hiconapi.h"
 #include "hicongraphicsitem.h"
-
-class HIconItemGroup : public HIconGraphicsItem
+class HBaseObj;
+class HGroupObj;
+class QObject;
+class QRectF;
+class QPainterPath;
+class QPointF;
+class QPainter;
+class QStyleOptionGraphicsItem;
+class QKeyEvent;
+class QGraphicsSceneMouseEvent;
+class H5ICONGUI_EXPORT HIconItemGroup : public HIconGraphicsItem
 {
 public:
-    HIconItemGroup();
+    enum {Type = enumGroup};
+
+    HIconItemGroup(HIconGraphicsItem *parent = Q_NULLPTR);
+    HIconItemGroup(const QRectF &rectF, HIconGraphicsItem *parent = Q_NULLPTR);
 public:
+    virtual void setRect(const QRectF& rect);
+    virtual QRectF rect() const;
+    void refreshBaseObj(const QRectF& rect);
+
     virtual QRectF boundingRect() const;
     //virtual bool contains(const QPointF &point) const;
     virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = Q_NULLPTR);
@@ -16,15 +34,20 @@ public:
     virtual void mousePressEvent(QGraphicsSceneMouseEvent *event);
     virtual void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
     virtual void keyPressEvent(QKeyEvent *event);
-public:
-    virtual void setRect(const QRectF& rect);
-    virtual QRectF rect()const;
+
     virtual void setItemObj(HBaseObj*);
     virtual HBaseObj* getItemObj();
     virtual ushort pointInRect(QPointF& point);
     virtual void setItemCursor(int position);
     virtual void moveItemBy(qreal dx,qreal dy);
     virtual void resizeItem(const QPolygonF& polygonF);
+
+private:
+    QRectF rectF;
+    HGroupObj* pGroupObj;
+    ushort rectMode;
+    ushort pointLocation;
+    QPointF pointStart;
 };
 
 #endif // HICONITEMGROUP_H
